f509321434e7e19f9fdfe3758b15e680f1136b58
DarylMurphyCoder <darylmurphy875@gmail.com>
2025-11-10 15:15:08 +0000

    set up structure for game functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,17 @@
+function runGame() {}
 
+function checkAnswer() {}
+
+function calculateCorrectAnswer() {}
+
+function incrementScore() {}
+
+function incrementWrongAnswers() {}
+
+function displayAdditionQuestin() {}
+
+function displaySubtractQuestion() {}
+
+function displayMultiplyQuestion() {}
+
+function displayDivisionQuestion() {}
